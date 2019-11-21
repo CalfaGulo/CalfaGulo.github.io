a63d61851fa8063161c0bcd146aefc0bf405d8b0
= <=>
2019-11-21 13:36:26 +0700

    Hallo Kakak Asdos

---
+++ precache-manifest.ef6cbf060ed45a542e8e2c63b44f99fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9405b8add399b339277e",
+    "url": "/css/about.3b3c9ddb.css"
+  },
+  {
+    "revision": "a43c3268c6df13fd71a1",
+    "url": "/css/chunk-vendors.41c9e408.css"
+  },
+  {
+    "revision": "6cf84ab5d00b15fd2fe3a0c5a1dcb082",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9405b8add399b339277e",
+    "url": "/js/about.d34fc018.js"
+  },
+  {
+    "revision": "c2d86b7a6322ef488c4d",
+    "url": "/js/app.1e8ed7f8.js"
+  },
+  {
+    "revision": "a43c3268c6df13fd71a1",
+    "url": "/js/chunk-vendors.52e2efbb.js"
+  },
+  {
+    "revision": "72c22c8e3717afe36bb47759c7296325",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
